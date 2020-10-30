5a6852ef0e286cd95dea209f50bd3efbf3d3a22a
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2020-10-29 23:51:28 -0700

    Added js functionality to my Timer and High Score data entry.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,13 @@
 // VARIABLES to pull from html
-var startQuiz = document.querySelector("#start")
-// var initialInput = document.querySelector("#initials")
+var startQuiz    = document.querySelector("#start");
+var initialInput = document.querySelector("#initials");
+var finalScore   = document.querySelector("#score");
+var submitButton = document.querySelector("#submit");
+var timeLeft     = document.getElementById("time-left")
+var quizIndex    = 0;
+var highScores   = [];
+var interval;
+
 
 // CREATE a function to display each container
 // https://www.w3schools.com/jsref/event_onpagehide.asp
@@ -16,34 +23,116 @@ var startQuiz = document.querySelector("#start")
 // }
 
 // render displayOutput();
-// CREATE function to beginQuestion
-function showQuestion() {
-    var question1 = document.getElementById("questions")
 
-    question1.innerHTML = "What is a DOM?";
+// CREATE a function with an array of variable questions
+var quiz = [
+    {
+        question: "What is DOM?",
+        options: ["Decree Operation Manual",
+                  "Document Object Manual",
+                  "Document Object Model",
+                  "Direct Object Model"],
+        answer: "Document Object Model"
+    },
+    {
+        question: "What is JSON?",
+        options: ["Decree Operation Manual",
+                  "Document Object Manual",
+                  "Document Object Model",
+                  "Direct Object Model"],
+        answer: "Document Object Model"
+    },
+    {
+        question: "What is DOM?",
+        options: ["Decree Operation Manual",
+                  "Document Object Manual",
+                  "Document Object Model",
+                  "Direct Object Model"],
+        answer: "Document Object Model"
+    },
+    {
+        question: "What is DOM?",
+        options: ["Decree Operation Manual",
+                  "Document Object Manual",
+                  "Document Object Model",
+                  "Direct Object Model"],
+        answer: "Document Object Model"
+    },
+]
 
-    var answer1 = document.getElementById("1");
 
-    answer1.innerHTML = "1. Decree Operation Manual";
 
-    var answer2 = document.getElementById("2");
+// CREATE a function to showQuestion
+function beginQuiz() {
+    startTimer();
+    // Assign question located from "questions" in html
+    var question = document.getElementById("questions")
 
-    answer2.innerHTML = "2. Document Object Manual";
+    // Assign text to show on H1
+    question.innerHTML = quiz[quizIndex].question;
 
-    var answer3 = document.getElementById("3");
+    // Get element buttons from html
+    // TODO: Consider to change into a for loop
+    var optionA = document.getElementById("1");
+    var optionB = document.getElementById("2");
+    var optionC = document.getElementById("3");
+    var optionD = document.getElementById("4");
 
-    answer3.innerHTML = "3. Document Object Model"
+    // Assign text to show on buttons
+    optionA.innerHTML = quiz[quizIndex].options[0];
+    optionB.innerHTML = quiz[quizIndex].options[1];
+    optionC.innerHTML = quiz[quizIndex].options[2];
+    optionD.innerHTML = quiz[quizIndex].options[3];
 
-    var answer4 = document.getElementById("4");
 
-    answer4.innerHTML = "4. Direct Object Model"
 
-        console.log(question1);
-        console.log(answer1);
-        console.log(answer2);
-        console.log(answer3);
-        console.log(answer4);
+
+        console.log(question);
+        console.log(options);
+
 }
 
+// SET and START timer
+function startTimer() {
+    resetTime();
+    interval = setInterval(function() {
+        totalSeconds--;
+        renderTime();
+      }, 1000);
+}
+
+function resetTime() {
+    clearInterval(interval);
+    totalSeconds = 75;
+}
+
+function renderTime() {
+    timeLeft.innerHTML = totalSeconds
+
+    if (totalSeconds === 0){
+        resetTime();
+    }
+}
+
+
+// SAVE and REDIRECT highScore to a new High Score Page
+function submitHighScore() {
+    // CLEAR initialInput list element and update finalScore
+    var initialsText = initialInput.value.trim();
+
+    console.log(initialsText);
+
+    highScores.push(initialsText);
+    initialInput.value = "";
+
+    localStorage.setItem("highScores", highScores);
+
+    window.location.href = "highscore.html"
+    // finalScore.textContent = initialInput;
+}
+
+// ADD .eventListener to 'submit' button
+submitButton.addEventListener("click", submitHighScore)
+
 // ADD .eventListener to 'start' button
-startQuiz.addEventListener("click", showQuestion)
+startQuiz.addEventListener("click", beginQuiz)
