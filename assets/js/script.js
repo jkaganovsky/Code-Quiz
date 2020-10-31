a64af27b7770b0df5fc72db4246dadbcd24322a6
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2020-10-31 00:13:33 -0700

    Added multiple functions to pages, timers, and quiz.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,15 @@
 // VARIABLES to pull from html
+
+// HIDE quiz section and final score section when page loads.
+var hideQuiz = document.getElementById("quiz-questions");
+var hideResults = document.getElementById("quiz-results");
+
+hideQuiz.style.display = "none";
+hideResults.style.display = "none";
+
+
 var startQuiz    = document.querySelector("#start");
-var initialInput = document.querySelector("#initials");
-var finalScore   = document.querySelector("#score");
-var submitButton = document.querySelector("#submit");
-var timeLeft     = document.getElementById("time-left")
 var quizIndex    = 0;
-var highScores   = [];
-var interval;
-
-
-// CREATE a function to display each container
-// https://www.w3schools.com/jsref/event_onpagehide.asp
-// function displayOutput() {
-//     var container = document.getElementsByClassName("container");
-
-//     if (container.style.display === "none") {
-//         container.style.display = "block";
-//     } else {
-//         container.style.display = "none";
-//     }
-//     console.log(container);
-// }
-
-// render displayOutput();
 
 // CREATE a function with an array of variable questions
 var quiz = [
@@ -32,7 +19,7 @@ var quiz = [
                   "Document Object Manual",
                   "Document Object Model",
                   "Direct Object Model"],
-        answer: "Document Object Model"
+        answer: "3"
     },
     {
         question: "What is JSON?",
@@ -40,59 +27,105 @@ var quiz = [
                   "Document Object Manual",
                   "Document Object Model",
                   "Direct Object Model"],
-        answer: "Document Object Model"
+        answer: "2"
     },
     {
-        question: "What is DOM?",
+        question: "Q3?",
         options: ["Decree Operation Manual",
                   "Document Object Manual",
                   "Document Object Model",
                   "Direct Object Model"],
-        answer: "Document Object Model"
+        answer: "4"
     },
     {
-        question: "What is DOM?",
+        question: "Q4?",
         options: ["Decree Operation Manual",
                   "Document Object Manual",
                   "Document Object Model",
                   "Direct Object Model"],
-        answer: "Document Object Model"
+        answer: "1"
     },
 ]
 
-
-
-// CREATE a function to showQuestion
+// CREATE a function to showQuestion when start button is clicked
 function beginQuiz() {
     startTimer();
+    showQuestion();
+
+    // SHOW Quiz Page
+    var showQuiz = document.getElementById("quiz-questions")
+
+    showQuiz.style.display = "block";
+
+    // HIDE Main Page
+    var hideMain = document.getElementById("welcome")
+
+    hideMain.style.display = "none";
+}
+
+// ADD .eventListener to 'start' button
+startQuiz.addEventListener("click", beginQuiz)
+
+// DEFINE a function to showQuestion.
+function showQuestion() {
     // Assign question located from "questions" in html
     var question = document.getElementById("questions")
-
+    console.log("quizIndex: " + quizIndex);
     // Assign text to show on H1
-    question.innerHTML = quiz[quizIndex].question;
+    question.textContent = quiz[quizIndex].question;
 
     // Get element buttons from html
-    // TODO: Consider to change into a for loop
     var optionA = document.getElementById("1");
     var optionB = document.getElementById("2");
     var optionC = document.getElementById("3");
     var optionD = document.getElementById("4");
 
     // Assign text to show on buttons
-    optionA.innerHTML = quiz[quizIndex].options[0];
-    optionB.innerHTML = quiz[quizIndex].options[1];
-    optionC.innerHTML = quiz[quizIndex].options[2];
-    optionD.innerHTML = quiz[quizIndex].options[3];
-
-
-
-
-        console.log(question);
-        console.log(options);
-
+    optionA.textContent = quiz[quizIndex].options[0];
+    optionB.textContent = quiz[quizIndex].options[1];
+    optionC.textContent = quiz[quizIndex].options[2];
+    optionD.textContent = quiz[quizIndex].options[3];
 }
 
-// SET and START timer
+var optionsQuiz  = document.querySelector("#options");
+
+// IF an option is clicked, a new question is shown
+function updateStats(event) {
+    console.log(event.target);
+    console.log(event.target.id);
+
+    quizIndex++;    //Moves index to the next question
+    if (quizIndex === quiz.length) {
+        stopQuiz();
+        return
+    }
+    showQuestion();
+}
+
+// ADD .eventListener to 'options' button
+optionsQuiz.addEventListener("click", updateStats)
+
+function stopQuiz() {
+    // SHOW Final Score Page
+    var showResults = document.getElementById("quiz-results");
+
+    showResults.style.display = "block";
+
+    // HIDE Quiz Page
+    var hideQuiz = document.getElementById("quiz-questions");
+
+    hideQuiz.style.display = "none";
+
+    // UPDATE FinalScore on the page.
+    finalScore.innerHTML = totalSeconds
+
+    // STOP the timer
+    stopTimer();
+}
+
+var interval;
+
+// SET, START, RESET, and STOP timer
 function startTimer() {
     resetTime();
     interval = setInterval(function() {
@@ -106,14 +139,26 @@ function resetTime() {
     totalSeconds = 75;
 }
 
+var timeLeft     = document.querySelector("#time-left");
+
 function renderTime() {
     timeLeft.innerHTML = totalSeconds
 
     if (totalSeconds === 0){
-        resetTime();
+        stopQuiz();
     }
 }
 
+function stopTimer() {
+    clearInterval(interval);
+    renderTime();
+}
+
+// TODO: Create a function when submitting your #initials.
+var initialInput = document.querySelector("#initials");
+var finalScore   = document.querySelector("#score");
+var submitButton = document.querySelector("#submit");
+var highScores   = [];
 
 // SAVE and REDIRECT highScore to a new High Score Page
 function submitHighScore() {
@@ -123,16 +168,36 @@ function submitHighScore() {
     console.log(initialsText);
 
     highScores.push(initialsText);
+
     initialInput.value = "";
 
-    localStorage.setItem("highScores", highScores);
+    finalScore.textContent = initialInput;
 
-    window.location.href = "highscore.html"
-    // finalScore.textContent = initialInput;
+    location.assign("highscore.html");
+
+    localStorage.setItem("highScores", highScores);
 }
 
 // ADD .eventListener to 'submit' button
 submitButton.addEventListener("click", submitHighScore)
 
-// ADD .eventListener to 'start' button
-startQuiz.addEventListener("click", beginQuiz)
+
+
+
+
+// FUNCTION to set when an answer is clicked
+// ALERT when answer is right or wrong
+// function optionsButton(event) {
+//     var optionsClickContent = event.target.textContent;
+//     var optionsValueContent = event.target.getAttribute("quiz", quizIndex)
+//     event.target.matches("button")
+
+//     // CREATE a function to display each .container on their own page
+//     let selectedBtn;
+
+//     optionsQuiz.onclick = function(event) {
+//         let optionsClickContent = event.target.textContent;
+//         if (target.textContent != quiz['answer'],
+//             alert("CORRECT!")
+// }
+
